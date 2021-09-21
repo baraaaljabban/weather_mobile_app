5620f545dc357e70fcd02a1865295a74dd446b20
Baraa Aljabban <baraa.aljabban.it@gmail.com>
2021-09-21 17:32:43 +0800

    add Bloc Delegate and my phone Settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'core/Service/injection_service.dart' as di;
 import 'app.dart';
+import 'core/simple_bloc_delegate.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await di.init();
+  Bloc.observer = SimpleBlocDelegate();
+
   runApp(const App());
 }
