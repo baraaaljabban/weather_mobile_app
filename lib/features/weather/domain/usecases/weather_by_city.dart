aea5b9b05a37530c4da8b6047129f6165bac40f7
Baraa Aljabban <baraa.aljabban.it@gmail.com>
2021-09-21 13:36:40 +0800

    add Weather Repository

--- lib/features/weather/domain/usecases/weather_by_city.dart
+++ lib/features/weather/domain/usecases/weather_by_city.dart
@@ -4,24 +4,25 @@ import 'package:dartz/dartz.dart';
 import 'package:weather/core/UseCases/use_cases.dart';
 import 'package:weather/core/error/failures.dart';
 import 'package:weather/features/weather/domain/entities/weather.dart';
+import 'package:weather/features/weather/domain/repositories/weather_repository.dart';
 
-class SearchWeatherByCitiy extends UseCase<List<Weather>, SearchWeatherByCitiyParams> {
+class SearchWeatherByCity extends UseCase<List<Weather>, SearchWeatherByCityParams> {
   final WeatherRepository repository;
-  SearchWeatherByCitiy({
+  SearchWeatherByCity({
     required this.repository,
   });
 
   @override
   Future<Either<Failure, List<Weather>>> call({
-    required SearchWeatherByCitiyParams params,
+    required SearchWeatherByCityParams params,
   }) async {
     return await repository.searchWeatherByCitiy(query: params.query);
   }
 }
 
-class SearchWeatherByCitiyParams {
+class SearchWeatherByCityParams {
   final String query;
-  SearchWeatherByCitiyParams({
+  SearchWeatherByCityParams({
     required this.query,
   });
 }
--- lib/features/weather/export_weather_feature.dart
+++ lib/features/weather/export_weather_feature.dart
@@ -2,4 +2,4 @@ export 'package:weather/features/weather/presentation/bloc/weather_bloc.dart';
 export 'package:weather/features/weather/data/datasource/weather_remote_data_source.dart';
 export 'package:weather/features/weather/data/models/weather_by_woeid_response_model.dart';
 export 'package:weather/features/weather/domain/repositories/weather_repository.dart';
-export 'package:weather/features/weather/domain/usecases/weatheruc.dart';
+export 'package:weather/features/weather/domain/usecases/weather_by_city.dart';
