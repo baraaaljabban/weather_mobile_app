2e1623651e782043bbf785416d9d55afe48d0afd
Baraa Aljabban <baraa.aljabban.it@gmail.com>
2021-09-22 22:48:44 +0800

    Implement Horizontal weather controller

--- lib/features/weather/presentation/widgets/horizontal/weather_controller_horizontal.dart
+++ lib/features/weather/presentation/widgets/horizontal/weather_controller_horizontal.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather/features/weather/domain/usecases/weather_by_city.dart';
 import 'package:weather/features/weather/presentation/bloc/weather_bloc.dart';
 import 'package:weather/features/weather/presentation/widgets/horizontal/detailed_weather_controller.dart';
 import 'package:weather/features/weather/presentation/widgets/horizontal/weather_days_list_controller.dart';
@@ -12,6 +13,8 @@ class WeatherControllerHorizontal extends StatefulWidget {
 }
 
 class _WeatherControllerHorizontalState extends State<WeatherControllerHorizontal> {
+  final myController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<WeatherBloc, WeatherState>(
@@ -24,17 +27,61 @@ class _WeatherControllerHorizontalState extends State<WeatherControllerHorizonta
       },
       listener: (context, state) {},
       builder: (context, state) {
-        if (state is DetailedDayWeatherState) {
-          return Column(
-            children: [
-              Expanded(flex: 5, child: DetailedWeatherController(consolidatedWeather: state.consolidatedWeather)),
-              Expanded(flex: 2, child: WeatherDaysListController(list: state.list)),
-            ],
-          );
-        } else {
-          return Container();
-        }
+        return Column(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Search(myController: myController),
+              ),
+            ),
+            if (state is DetailedDayWeatherState)
+              Expanded(flex: 10, child: DetailedWeatherController(consolidatedWeather: state.consolidatedWeather)),
+            if (state is DetailedDayWeatherState) Expanded(flex: 5, child: WeatherDaysListController(list: state.list)),
+          ],
+        );
       },
     );
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the
+    // widget tree.
+    myController.dispose();
+    super.dispose();
+  }
+
+  void dispatchVerifyEvent() {
+    BlocProvider.of<WeatherBloc>(context).add(SearchWeatherByCityEvent(params: SearchWeatherByCityParams(query: myController.text)));
+  }
+}
+
+class Search extends StatelessWidget {
+  const Search({
+    Key? key,
+    required this.myController,
+  }) : super(key: key);
+
+  final TextEditingController myController;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: Colors.white,
+      controller: myController,
+      decoration: const InputDecoration(
+        suffixIcon: Icon(Icons.search),
+        border: OutlineInputBorder(),
+        hintText: 'search your city',
+      ),
+      textInputAction: TextInputAction.done,
+      onSubmitted: (value) => dispatchVerifyEvent(context),
+    );
+  }
+
+  void dispatchVerifyEvent(BuildContext context) {
+    BlocProvider.of<WeatherBloc>(context).add(SearchWeatherByCityEvent(params: SearchWeatherByCityParams(query: myController.text)));
   }
 }
