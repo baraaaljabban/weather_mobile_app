0580538dd3f3885f80a1c766101a21cd69477d25
Baraa Aljabban <baraa.aljabban.it@gmail.com>
2021-09-22 22:49:06 +0800

    Detailed Weather Controller

--- lib/features/weather/presentation/widgets/horizontal/detailed_weather_controller.dart
+++ lib/features/weather/presentation/widgets/horizontal/detailed_weather_controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather/core/AppStrings/server_strings.dart';
 import 'package:weather/features/weather/domain/entities/weather.dart';
 import 'package:weather/features/weather/presentation/bloc/weather_bloc.dart';
 
@@ -25,16 +26,96 @@ class _DetailedWeatherControllerState extends State<DetailedWeatherController> {
       listener: (context, state) {},
       builder: (context, state) {
         if (state is DetailedDayWeatherState) {
-          return Column(
-            children: [
-              Text("${widget.consolidatedWeather.day}"),
-              Text("${widget.consolidatedWeather.humidity}"),
-              Text("${widget.consolidatedWeather.theTemp}"),
-            ],
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Center(
+                    child: Text(
+                      widget.consolidatedWeather.day,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Text(
+                    widget.consolidatedWeather.weatherStateName,
+                  ),
+                ),
+                Expanded(
+                  flex: 5,
+                  child: Center(
+                      child: MyNetworkImage(
+                        weatherStateAbbr: widget.consolidatedWeather.weatherStateAbbr,
+                      ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Center(
+                    child: Text(
+                      "${widget.consolidatedWeather.theTemp}\u2103",
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Text(
+                    "Humidity: ${widget.consolidatedWeather.humidity}%",
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Text(
+                    "Pressure : ${widget.consolidatedWeather.airPressure} hpa",
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Text(
+                    "Wind: ${widget.consolidatedWeather.windSpeed} km/h",
+                  ),
+                ),
+              ],
+            ),
           );
         } else {
           return Container();
         }
+      },
+    );
+  }
+}
+
+class MyNetworkImage extends StatelessWidget {
+  const MyNetworkImage({
+    Key? key,
+    required this.weatherStateAbbr,
+    this.width,
+    this.height,
+  }) : super(key: key);
+  final String weatherStateAbbr;
+  final double? width;
+  final double? height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      IMAGE_URL + weatherStateAbbr + '.png',
+      alignment: Alignment.center,
+      fit: BoxFit.contain,
+      width: width,
+      height: height,
+      loadingBuilder: (context, child, loadingProgress) {
+        if (loadingProgress == null) {
+          return child;
+        }
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
       },
     );
   }
