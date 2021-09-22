06e8d2bddffb0a1cc2d41e6e0b9c5ddf4748e7c1
Baraa Aljabban <baraa.aljabban.it@gmail.com>
2021-09-22 22:50:27 +0800

    fix for pull to refresh search params

--- lib/features/weather/presentation/bloc/weather_event.dart
+++ lib/features/weather/presentation/bloc/weather_event.dart
@@ -3,9 +3,11 @@ part of 'weather_bloc.dart';
 @immutable
 abstract class WeatherEvent {}
 
+///this event is for the user to input the city name
+///using`[params]` `SearchWeatherByCityParams`
 class SearchWeatherByCityEvent extends WeatherEvent {
-  final SearchWeatherByCityParams params;
-  SearchWeatherByCityEvent({required this.params});
+  final SearchWeatherByCityParams? params;
+  SearchWeatherByCityEvent({this.params});
 }
 
 ///this even it to call when user select another day to show it's infromation in deatils
